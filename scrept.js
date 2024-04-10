b3fc869887a976b83a4b1f1dbdfc1188455094b9
Dillippanda <118836414+Dillippanda@users.noreply.github.com>
2024-04-10 19:49:39 +0530

    Update scrept.js

--- scrept.js
+++ scrept.js
@@ -5,6 +5,7 @@ const fromCurr = document.querySelector(".from select ")
 const toCurr = document.querySelector(".to select ")
 let msg = document.querySelector(".msg")
 
+
 for(let select of dropdowns){
     for(currCode in countryList){
         let newOption=document.createElement("option")
@@ -23,21 +24,14 @@ select.append(newOption)
 updateFlag(evt.target);
     });
 }
-const updateFlag=(element)=>{
-let currCode= element.value;
-let cuntryCode=countryList[currCode];
-let newSrc = `https://flagsapi.com/${cuntryCode}/flat/64.png`;
-let imgl =element.parentElement.querySelector("img");
-imgl.src = newSrc;
-};
-btn.addEventListener("click" , (evt)=>{
-    evt.preventDefault();
+const update = async()=>{
     let amount = document.querySelector(".amount input");
     let amtVal = amount.value;
    if ( !(amtVal >=0 || amtVal== NaN) ){
 alert("Enter a valid  input");
     }
 else{
+    
     const URL=`https://open.er-api.com/v6/latest/${fromCurr.value}`
 
 
@@ -51,5 +45,19 @@ msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`
     }
     getcurrency();
 }
+}
+const updateFlag=(element)=>{
+let currCode= element.value;
+let cuntryCode=countryList[currCode];
+let newSrc = `https://flagsapi.com/${cuntryCode}/flat/64.png`;
+let imgl =element.parentElement.querySelector("img");
+imgl.src = newSrc;
+};
+window.addEventListener("load",()=>{
+    update();
+})
+btn.addEventListener("click" , (evt)=>{
+    evt.preventDefault();
+update();
 
 })
